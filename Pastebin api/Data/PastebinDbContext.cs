853d50aa3abe0c304d7acb23e4a2e39b3c61c2cd
TeymurAsk <teymurasgar@gmail.com>
2024-10-31 12:26:18 +0100

    Add project files.

---
+++ Pastebin api/Data/PastebinDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Pastebin_api.Data
+{
+    public class PastebinDbContext : DbContext
+    {
+        public PastebinDbContext(DbContextOptions<PastebinDbContext> options) : base(options)
+        {
+
+        }
+        public List<TextBlock> TextBlocks { get; set; }
+    }
+}
---
+++ Pastebin api/Data/TextBlock.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Pastebin_api.Data
+{
+    public class TextBlock
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Text { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Title { get; set; }
+
+    }
+}
