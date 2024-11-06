70f78159914b71ed60dabfd2ddff5417cf3941ae
TeymurAsk <teymurasgar@gmail.com>
2024-11-06 16:56:32 +0100

    last

--- Pastebin api/Program.cs
+++ Pastebin api/Program.cs
@@ -93,6 +93,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI(c =>
     {
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "Pastebin API V1");
+        c.RoutePrefix = string.Empty;
     });
 }
 
